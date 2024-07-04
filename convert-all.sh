3e325dc91444cc879d52d91c710cc95477d6b635
ethanc8 <echaroenpitaks@gmail.com>
2024-07-04 13:34:24 -0500

    Add easy conversion script

---
+++ convert-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mamba env create -f environment-cpu.yml
+mamba activate Gemini-Nano
+wget https://huggingface.co/oongaboongahacker/Gemini-Nano/resolve/main/weights.bin
+
+cd playground
+python3 converter.py ../weights.bin ../weights_fp32.safetensors
+cd ..
