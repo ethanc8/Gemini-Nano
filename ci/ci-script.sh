c56650278bd97aba48aff13b397c2342db1e27e7
ethanc8 <echaroenpitaks@gmail.com>
2024-07-04 19:18:34 -0500

    Run mamba init before running convert-all.sh

--- ci/ci-script.sh
+++ ci/ci-script.sh
@@ -4,6 +4,8 @@ wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforg
 bash Miniforge3-$(uname)-$(uname -m).sh -b
 rm Miniforge3-$(uname)-$(uname -m).sh
 
+PATH="$HOME/miniforge3/condabin/:$PATH" mamba init
+
 PATH="$HOME/miniforge3/condabin/:$PATH" bash convert-all.sh $1
 
 mv weights_$1.safetensors Gemini-Nano-CI/weights_$1.safetensors
