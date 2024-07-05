3b837b472c2c316262b78f9a825bea3c9a6ea4d5
ethanc8 <echaroenpitaks@gmail.com>
2024-07-04 19:24:38 -0500

    Make CI logs less verbose and more helpful

--- ci/ci-script.sh
+++ ci/ci-script.sh
@@ -1,6 +1,8 @@
+echo "Cloning https://huggingface.co/ethanc8/Gemini-Nano-CI..."
 GIT_LFS_SKIP_SMUDGE=1 git clone https://ethanc8:${HF_TOKEN}@huggingface.co/ethanc8/Gemini-Nano-CI
 
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+echo "Installing Miniforge3..."
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh" --no-verbose
 bash Miniforge3-$(uname)-$(uname -m).sh -b
 rm Miniforge3-$(uname)-$(uname -m).sh
 
@@ -9,9 +11,11 @@ source ~/.bashrc
 mamba init
 bash convert-all.sh $1
 
+echo "Moving weights to Gemini-Nano-CI..."
 mv weights_$1.safetensors Gemini-Nano-CI/weights_$1.safetensors
 
 cd Gemini-Nano-CI
+echo "Commiting and pushing weights to Gemini-Nano-CI..."
 git add weights_$1.safetensors && git commit -m "Upload of $1 weights"
 git push
 cd ..
--- convert-all.sh
+++ convert-all.sh
@@ -2,8 +2,10 @@
 
 mamba env create -f environment-cpu.yml
 mamba activate Gemini-Nano
-wget https://huggingface.co/oongaboongahacker/Gemini-Nano/resolve/main/weights.bin
+echo "Downloading weights..."
+wget https://huggingface.co/oongaboongahacker/Gemini-Nano/resolve/main/weights.bin --no-verbose
 
+echo "Converting weights..."
 cd playground
 python3 converter.py ../weights.bin ../weights_$1.safetensors
 cd ..
