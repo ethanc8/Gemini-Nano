5f51a59beded9d7b1ee944d932f74a470886463d
ethanc8 <echaroenpitaks@gmail.com>
2024-07-05 08:54:04 -0500

    Install miniforge3 with setup-miniconda action instead

--- ci/ci-script.sh
+++ ci/ci-script.sh
@@ -1,27 +1,6 @@
 echo "Cloning https://huggingface.co/ethanc8/Gemini-Nano-CI..."
 GIT_LFS_SKIP_SMUDGE=1 git clone https://ethanc8:${HF_TOKEN}@huggingface.co/ethanc8/Gemini-Nano-CI
 
-echo "Installing Miniforge3..."
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh" --no-verbose
-bash Miniforge3-$(uname)-$(uname -m).sh -b
-rm Miniforge3-$(uname)-$(uname -m).sh
-
-__conda_setup="$('$HOME/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$HOME/miniforge3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/miniforge3/etc/profile.d/conda.sh"
-    else
-        export PATH="$HOME/miniforge3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-if [ -f "$HOME/miniforge3/etc/profile.d/mamba.sh" ]; then
-    . "$HOME/miniforge3/etc/profile.d/mamba.sh"
-fi
-
 bash convert-all.sh $1
 
 echo "Moving weights to Gemini-Nano-CI..."
--- convert-all.sh
+++ convert-all.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mamba env create -f environment-cpu.yml
-conda activate Gemini-Nano
 echo "Downloading weights..."
 wget https://huggingface.co/oongaboongahacker/Gemini-Nano/resolve/main/weights.bin --no-verbose
 
