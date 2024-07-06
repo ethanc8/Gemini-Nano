31e0d5618aceb75465ab12c91e00bd46fd545fbb
ethanc8 <echaroenpitaks@gmail.com>
2024-07-05 22:23:22 -0500

    Activate the environment with conda, and re-call the conda hook

--- convert-all.sh
+++ convert-all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+eval "$(~/miniforge3/bin/conda shell.bash hook)"
 mamba env create -f environment-cpu.yml
-mamba activate Gemini-Nano
+conda activate Gemini-Nano
 
 echo "Downloading weights..."
 wget https://huggingface.co/oongaboongahacker/Gemini-Nano/resolve/main/weights.bin --no-verbose
